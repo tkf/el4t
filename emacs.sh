549dffecfead54734662b340ac2735f3a55413e8
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-22 21:51:58 +0100

    Update git submodule in emacs.sh

--- emacs.sh
+++ emacs.sh
@@ -22,6 +22,10 @@
 # along with EL4T.
 # If not, see <http://www.gnu.org/licenses/>.
 
+set -e
+# Download lib/ert/ if necessary
+git submodule update --init
+
 EL4T_DIR=$(dirname $0)
 EL4T_EMACS="${EL4T_EMACS:-emacs}"
 exec $EL4T_EMACS -l $EL4T_DIR/lib/el4t.el "$@"
