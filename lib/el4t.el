cdd8c69729dd44267271ae7c946e503b81a92b65
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-22 20:13:33 +0100

    Add header comments

--- lib/el4t.el
+++ lib/el4t.el
@@ -1,4 +1,32 @@
-;; Library load helper
+;;; el4t.el --- Library load helper
+
+;; Copyright (C) 2012 Takafumi Arakaki
+
+;; Author: Takafumi Arakaki <aka.tkf at gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; EL4T is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; EL4T is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with EL4T.
+;; If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Load package.el and ert.el if they are not included in the Emacs
+;; loading this script.  Do nothing if they can be loaded.  This is a
+;; helper script for ../emacs.sh.
+
+;;; Code:
 
 (defvar el4t-lib (file-name-directory load-file-name))
 
@@ -13,3 +41,5 @@
 
 (unless (fboundp 'ert-run-tests-interactively)
   (autoload 'ert-run-tests-interactively "ert-ui"))
+
+;;; el4t.el ends here
